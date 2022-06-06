5aee2309378f4e20afd14a83b531b7e1bf9109c5
João Gabriel <jgabrielfes@gmail.com>
2022-06-05 22:38:11 -0300

    Página do trabalho detalhada finalizada :hot_face:

--- src/pages/WorkInfo.jsx
+++ src/pages/WorkInfo.jsx
@@ -1,29 +1,137 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import Chip from '@mui/material/Chip';
+import Grow from '@mui/material/Grow';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Slide from '@mui/material/Slide';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import MDialog from '../components/MDialog';
 
 import { collaborations, works, trainingProjects } from '../utils/works';
 
 const ALL_WORKS = [...collaborations, ...works, ...trainingProjects];
 
 class WorkInfo extends React.Component {
+  constructor() {
+    super();
+    this.handleDevTool = this.handleDevTool.bind(this);
+    this.handleCloseToolDialog = this.handleCloseToolDialog.bind(this);
+
+    this.state = {
+      toolDialogOpen: false,
+      selectedTool: {
+        title: '',
+        description: '',
+        icon: <></>,
+      },
+    };
+  }
+
   componentDidMount() {
     window.scrollTo(0, 0);
   }
 
+  handleDevTool(tool) {
+    this.setState({
+      toolDialogOpen: true,
+      selectedTool: {
+        title: tool.name,
+        description: tool.description,
+        icon: tool.svg,
+      },
+    });
+  }
+
+  handleCloseToolDialog() {
+    this.setState({ toolDialogOpen: false });
+  }
+
   render() {
-    const { match: { params: { work } } } = this.props;
+    const { history, match: { params: { work } } } = this.props;
+    const { toolDialogOpen, selectedTool } = this.state;
     const workInfo = ALL_WORKS.find(({ path }) => path === work);
 
     if (!workInfo) return <Redirect to="/works" />
 
     return (
-      <span>{work}</span>
+      <>
+        <Slide in direction="right" timeout={500}>
+          <Stack alignItems="center" component="section" direction="row" spacing={2} mb={5}>
+            <IconButton size="large" onClick={() => history.push('/works')}>
+              <ArrowBackIcon />
+            </IconButton>
+            <Stack>
+              <Typography variant="h3" noWrap>
+                {workInfo.title}
+              </Typography>
+              <Typography color="text.secondary">
+                {workInfo.date}
+              </Typography>
+            </Stack>
+          </Stack>
+        </Slide>
+
+        <Grow in timeout={1000}>
+          <Paper component="section" sx={{ mb: { xs: 2, sm: 3 }, mx: 'auto', p: 2, width: 'fit-content' }}>
+            <img
+              width="100%"
+              src={workInfo.image}
+              alt={workInfo.title}
+            />
+          </Paper>
+        </Grow>
+
+        <Grow in timeout={2000}>
+          <Paper component="section" sx={{ p: 2 }}>
+            <Typography align="justify" sx={{ mb: 2, textIndent: '1.2cm' }}>
+              {workInfo.text}
+            </Typography>
+
+            {workInfo.repo && (
+              <Stack alignItems="center" direction="row" spacing={1}>
+                <Typography variant="overline" sx={{ mt: 0.4 }}>
+                  Repositório:
+                </Typography>
+                <Link href={workInfo.repo} target="blank">{workInfo.repo}</Link>
+              </Stack>
+            )}
+
+            {workInfo.url && (
+              <Stack alignItems="center" direction="row" spacing={1}>
+                <Typography variant="overline" sx={{ mt: 0.4 }}>
+                  Deploy:
+                </Typography>
+                <Link href={workInfo.url} target="blank">{workInfo.url}</Link>
+              </Stack>
+            )}
+
+            <Stack alignItems="center" direction="row" spacing={0.5}>
+              <Typography variant="overline" sx={{ mr: 0.5, mt: 0.4 }}>
+                Tecnologias utilizadas:
+              </Typography>
+              {workInfo.devTools.map((tool) => (
+                <Chip key={tool} label={tool} />
+              ))}
+            </Stack>
+          </Paper>
+        </Grow>
+
+        <MDialog tool={selectedTool} open={toolDialogOpen} onClose={this.handleCloseToolDialog} />
+      </>
     );
   }
 }
 
 WorkInfo.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
   match: PropTypes.shape({
     params: PropTypes.shape({
       work: PropTypes.string,
--- src/utils/works.js
+++ src/utils/works.js
@@ -5,6 +5,8 @@ export const works = [
     date: '23 de maio de 2022',
     image: require('../images/yaris.jpg'),
     description: 'Sistema CRM desenvolvido com o intuito de auxiliar a gestão do relacionamento com o cliente.',
+    text: 'Descrição mais detalhada. Em construção...',
+    devTools: ['React', 'Redux', 'Material UI'],
   },
   {
     path: 'calcarios',
@@ -12,6 +14,8 @@ export const works = [
     date: '01 de junho de 2022',
     image: require('../images/calcarios.jpg'),
     description: 'Sistema para controle de gestão elaborado a partir de uma planilha.',
+    text: 'Descrição mais detalhada. Em construção...',
+    devTools: ['React', 'Redux', 'Material UI'],
   },
 ];
 
@@ -22,6 +26,9 @@ export const collaborations = [
     date: '04 de maio de 2022',
     image: require('../images/hrp.jpg'),
     description: 'Foi realizado a integração com o back-end do sistema, bem como a criação de novas funcionalidades.',
+    text: 'Descrição mais detalhada. Em construção...',
+    devTools: ['React', 'Material UI'],
+    url: 'http://pea.hrpservicos.com.br/',
   },
 ];
 
@@ -32,6 +39,10 @@ export const trainingProjects = [
     date: '24 de fevereiro de 2022',
     image: require('../images/clock.jpg'),
     description: 'Relógio analógico criado com o objetivo pessoal de treinar as habilidades de CSS.',
+    text: 'Descrição mais detalhada. Em construção...',
+    devTools: ['CSS', 'React', 'Material UI'],
+    repo: 'https://github.com/jgabrielfes/clock-project/',
+    url: 'https://clock-project-jgabrielfes.vercel.app/',
   },
   {
     path: 'password',
@@ -39,6 +50,10 @@ export const trainingProjects = [
     date: '30 de abril de 2022',
     image: require('../images/password.jpg'),
     description: 'Sistema de geração de senha através de alguns parâmetros inseridos pelo usuário.',
+    text: 'Descrição mais detalhada. Em construção...',
+    devTools: ['JavaScript', 'React', 'Material UI'],
+    repo: 'https://github.com/jgabrielfes/password-generator/',
+    url: 'https://password-generator-jgabrielfes.vercel.app/',
   },
   {
     path: 'recipes',
@@ -46,5 +61,9 @@ export const trainingProjects = [
     date: '13 de maio de 2022',
     image: require('../images/recipes.jpg'),
     description: 'Aplicativo de receitas feito em grupo para treinamento de trabalho em equipe.',
+    text: 'Descrição mais detalhada. Em construção...',
+    devTools: ['CSS', 'Jest', 'React'],
+    repo: 'https://github.com/jgabrielfes/recipes-app/',
+    url: 'https://recipes-trybe.vercel.app/',
   },
 ];
